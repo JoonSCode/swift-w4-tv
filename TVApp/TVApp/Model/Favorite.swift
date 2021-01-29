bcbecb90924b577322fa455698d680a30a17b319
eden.joon <eden.joon@kakaocorp.com>
2021-01-29 11:33:44 +0900

    즐겨찾기 객체구현, 관리 객체 구현

---
+++ TVApp/TVApp/Model/Favorite.swift
@@ -0,0 +1,18 @@
+//
+//  Favorite.swift
+//  TVApp
+//
+//  Created by 윤준수 on 2021/01/29.
+//
+
+import Foundation
+
+class Favorite {
+    var title = ""
+    var subTitle = ""
+    
+    init(title: String, subTitle: String) {
+        self.title = title
+        self.subTitle = subTitle
+    }
+}
